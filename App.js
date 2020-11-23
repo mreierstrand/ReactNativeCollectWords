14c1932dbd23a5394146b08784b8573ca48e46d6
oliv1115 <48084927+oliv1115@users.noreply.github.com>
2020-11-23 12:58:41 +0100

    Kan få userinput

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, {useState } from 'react';
+import { StyleSheet, Text, View, TextInput, SafeAreaView } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginTop: 20,
+    paddingTop: 0,
+    paddingHorizontal: 0,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'center', 
     justifyContent: 'center',
   },
+  textHeader: {
+    textAlign: 'center',
+    paddingTop: 0,
+    fontSize: 40,
+  },
+  textRegular: {
+    textAlign:"center",
+    fontSize: 20,
+    paddingTop: 100,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  result: {
+    padding: 8,
+    fontSize: 20
+  }
+
 });
+
+export default function App() {
+
+  const [name, setName] = useState('Name');
+  
+  
+  
+  return (
+    <SafeAreaView>
+    <View style={styles.container}>
+     <StatusBar style="dark" />
+    <Text style={styles.textHeader}>CollectWords</Text>
+    <Text style={styles.textRegular}>Enter a name</Text>
+    <TextInput 
+      placeholder='Type a name' 
+      style={styles.input}
+      onChangeText={(value) => setName(value)} />
+     <Text style={styles.result}>{name}</Text>
+    </View>
+    </SafeAreaView>
+  );
+}
+
+
